8207e9aaad728c7d8fac97ed105fff3494288f3f
shaharyarali <shaharyar4t4@gmail.com>
2026-02-22 00:38:30 +0500

    integration of Rate Limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { seconds, ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          name: 'defualt',
+          ttl: seconds(60),
+          limit: 3,
+        }
+      ],
+      errorMessage: 'Too many Request.. Please wait a minute and try again!'
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard
+  }],
 })
-export class AppModule {}
+export class AppModule { }
